71244de236e46b69cafffc68b26bc4aeecc6ff57
kevCRUD <97689028+kevCRUD@users.noreply.github.com>
2022-05-02 12:44:17 +0300

    tweaks

--- constants/data.js
+++ constants/data.js
@@ -6,48 +6,7 @@ const userData = {
   email: "kevowino2016@gmail.com",
   phone: "+254 701 802515",
   address: "Marcus Garvey Road, Nairobi.",
-  projects: [
-    {
-      title: "Tailwind Master Kit",
-      link: "https://tailwindmasterkit.com",
-      imgUrl: "/tmk.jpg",
-    },
-    {
-      title: "PlaceholderTech",
-      link: "https://placeholdertech.in",
-      imgUrl: "/placeholdertech.png",
-    },
-    {
-      title: "Portfolio",
-      link: "https://manuarora.in",
-      imgUrl: "/portfolio.png",
-    },
-    {
-      title: "CovidRescue",
-      link: "https://covidrescue.co.in",
-      imgUrl: "/covidrescue.png",
-    },
-    {
-      title: "Algochurn",
-      link: "https://algochurn.com",
-      imgUrl: "/algochurn.png",
-    },
-    {
-      title: "Playground",
-      link: "https://play.placeholdertech.in",
-      imgUrl: "/playground.png",
-    },
-    {
-      title: "Feedmeback",
-      link: "https://feedmeback.vercel.app",
-      imgUrl: "/feedmeback.png",
-    },
-    {
-      title: "Devmedium",
-      link: "https://devmedium.vercel.app",
-      imgUrl: "/devmedium.png",
-    },
-  ],
+  
   about: {
     title:
       "I'm a novice web developer trying to better my skills every single day",
@@ -56,57 +15,13 @@ const userData = {
       `I started off with "Java-Teach Yourself in 30 Days"...in 6 months I still couldn't get it. I switched to Javascript and then Python nailed it for me.`,
       `Now I'm coding in Python, React, Node.js, Express, MongoDB, MySQL, Bootstrap, Tailwind, to name a few.`,
     ],
-    currentProject: "Product Landing Page",
-    currentProjectUrl: "https://codepen.io/kevOWINO/pen/dyzdzKK",
+    currentProject: "Tours and Travel Landing Page",
+    currentProjectUrl: "https://port-florence-lake-tours.vercel.app/",
   },
-  experience: [
-    {
-      title: "Software Developer",
-      company: "mroads",
-      year: "2021",
-      companyLink: "https://mroads.com",
-      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "TailwindMasterKit",
-      year: "2021",
-      companyLink: "https://tailwindmasterkit.com",
-      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "Algochurn",
-      year: "2021",
-      companyLink: "https://algochurn.com",
-      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
-    },
-    {
-      title: "Internship",
-      company: "Intel Corporation",
-      year: "2019",
-      companyLink: "https://intel.com",
-      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
-    },
-    {
-      title: "Graduation",
-      company: "UPES, Dehradun.",
-      year: "2019",
-      companyLink: "https://upes.co.in",
-      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
-    },
-    {
-      title: "High School",
-      company: "Delhi Public School, Jaipur.",
-      year: "2015",
-      companyLink: "https://upes.co.in",
-      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
-    },
-  ],
   resumeUrl:
     "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
   socialLinks: {
-    twitter:  "https://twitter.com/DataDoc21",
+    twitter:  "https://twitter.com/kevCRUD",
     linkedin: "https://www.linkedin.com/in/kevin-o-248906155/",
     github:   "https://github.com/kevCRUD",
     facebook: "https://www.facebook.com/kevowino22/",
